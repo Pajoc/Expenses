03ad40e69ed05dc4eb3c5b8788f1328baf2c7abf
Pajoc <pajoc527@gmail.com>
2019-05-21 21:45:26 +0100

    Primeiro get

---
+++ ExpensesBE.API/Controllers/FamiliesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ExpensesBE.API.Controllers
+{
+    [Route("api/families")]
+    public class FamiliesController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetFamilies()
+        {
+            return new JsonResult(new List<object>() {
+                new {id = 1, Name = "Fuel" },
+                new {id = 2, Name = "Restaurant" },
+                new {id = 3, Name = "Hotel" }
+            });
+        }
+    }
+}
--- ExpensesBE.API/Startup.cs
+++ ExpensesBE.API/Startup.cs
@@ -15,6 +15,9 @@ namespace ExpensesBE.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // 1
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,13 @@ namespace ExpensesBE.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // 2
+            app.UseMvc();//handle HTTP request
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
