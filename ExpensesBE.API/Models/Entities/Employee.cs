0d3af4449a48f6bc057fb92f90a318e26bd662d5
Paulo Costa <pajoc527@hotmail.com>
2019-07-01 17:29:56 +0100

    Preparação

---
+++ ExpensesBE.API/Models/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesBE.API.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(5)]
+        public string Code { get; set; }
+
+        public decimal Treshold { get; set; }
+        [StringLength(50)]
+        [EmailAddress]
+        public string MainEmail { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public Guid? DepartmentId { get; set; }
+
+        public Department DepartmentOfEmployee { get; set; }
+    }
+}
