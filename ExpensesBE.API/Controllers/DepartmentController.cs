7ac3949ab28d2ed086f1a9e0aefba628b05dc1cb
Paulo Costa <pajoc527@hotmail.com>
2019-07-02 17:40:39 +0100

    GetDepartment

---
+++ ExpensesBE.API/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesBE.API.Controllers
+{
+    [Route("api/department")]
+    public class DepartmentController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetDepartment()
+        {
+            return new JsonResult(new List<object>() {
+                new {id = Guid.NewGuid(), Description = "Accounting" },
+                new {id = Guid.NewGuid(), Description = "Design" },
+                new {id = Guid.NewGuid(), Description = "IT" },
+                new {id = Guid.NewGuid(), Description = "Production" },
+            });
+        }
+    }
+}
